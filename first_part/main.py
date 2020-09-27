860786f9c334e20afc44fc6465800e8199bdfb1b
Kryukov.SA <sergey.krykov.2806@gmail.com>
2020-09-27 19:10:05 +0300

    30% complete

--- first_part/main.py
+++ first_part/main.py
@@ -1,6 +1,22 @@
 import numpy as np
 import cv2
 
+import random
 
 if __name__ == "__main__":
     print('hello world')
+    mode = int(input('mode:'))  # Считываем номер операции которую надо сделать
+    if mode == 0:
+        image = cv2.imread(r'C:\Users\HP-PC\Desktop\google.jpg')  # Открываем изображение r неизвестно зачем но работать так должно.
+        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        cv2.imshow('Original image', image)
+        cv2.imshow('Gray image', gray)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+
+
+        # blue, green, red = cv2.split(image)
+
+    # draw = ImageDraw.Draw(image)   Создаем инструмент для рисования.
+    # height = image.size[1]   Определяем высоту.
+    # pix = image.load()   Выгружаем значения пикселей
