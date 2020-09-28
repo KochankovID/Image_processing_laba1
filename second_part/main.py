6d0082a407d685c402ff9dabf2a16e8545cf5526
Kryukov.SA <sergey.krykov.2806@gmail.com>
2020-09-28 12:37:07 +0300

    correct some details

--- second_part/main.py
+++ second_part/main.py
@@ -2,36 +2,48 @@ import numpy as np
 import cv2
 import time
 
+def average (img):
+    b = img[:, :, 0]
+    g = img[:, :, 1]
+    r = img[:, :, 2]
+    single = (b + g + r) / 3
+    copy = img.copy()
+    copy[:, :, 0] = single = (b + g + r) / 3
+    copy[:, :, 1] = single = (b + g + r) / 3
+    copy[:, :, 2] = single = (b + g + r) / 3
+    return copy
+
+
+
 if __name__ == "__main__":
     print('hello world')
-    mode = int(input('mode:'))  # Считываем номер операции которую надо сделать
-    if mode == 0:  # b.	Реализовать конвертацию в оттенки серого при помощи cv::cvtColor().
-        image = cv2.imread(r'C:\Users\HP-PC\Desktop\google.jpg')  # Открываем изображение r неизвестно зачем но работать так должно.
+    mode = int(input('mode:'))
+
+    if mode == 0:
+        image = cv2.imread(r'src/google.jpg')
         start = time.time()
         gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
         end = time.time()
         print(end - start)
+
         cv2.imshow('Original image', image)
         cv2.imshow('Gray image', gray)
         cv2.waitKey(0)
         cv2.destroyAllWindows()
-    if mode == 1: #	Конвертация цветного изображения в монохромное изображение
-        image = cv2.imread(r'C:\Users\HP-PC\Desktop\google.jpg')
-      #  blue, green, red = cv2.split(image)  # Split the image into its channels
+
+
+    if mode == 1:
+        image = cv2.imread(r'src/google.jpg')
+
         start = time.time()
-        b = image[:, :, 0]
-        g = image[:, :, 1]
-        r = image[:, :, 2]
-        single = (b + g + r) / 3
-        #-----------------
-        copy = image.copy()
-        copy[:, :, 0] = single = (b + g + r) / 3
-        copy[:, :, 1] = single = (b + g + r)/3
-        copy[:, :, 2] = single = (b + g + r)/3
+        copy1 = average(image)
         end = time.time()
         print(end - start)
-        #-------------------
+        
         cv2.imshow('Original image', image)
-        cv2.imshow('average image', copy)
+        cv2.imshow('average image', copy1)
         cv2.waitKey(0)
         cv2.destroyAllWindows()
+
+
+
