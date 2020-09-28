29fa71bab7367172dc77083b1a3eb9c83751f0cd
Kryukov.SA <sergey.krykov.2806@gmail.com>
2020-09-28 21:30:20 +0300

    without def threewindow

--- second_part/main.py
+++ second_part/main.py
@@ -2,43 +2,55 @@ import numpy as np
 import cv2
 import time
 
-def average (img):
+import sys
+import os.path as osp
+
+
+def average(img):
     b = img[:, :, 0]
     g = img[:, :, 1]
     r = img[:, :, 2]
+
     single = (b + g + r) / 3
     copy = img.copy()
-    copy[:, :, 0] = single = (b + g + r) / 3
-    copy[:, :, 1] = single = (b + g + r) / 3
-    copy[:, :, 2] = single = (b + g + r) / 3
+
+    copy[:, :, 0] = single
+    copy[:, :, 1] = single
+    copy[:, :, 2] = single
+
     return copy
 
 
-
-if __name__ == "__main__":
-    print('hello world')
-
-    image = cv2.imread('src/google.jpg')
+def time_cv(image1):
     start = time.time()
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    gray = cv2.cvtColor(image1, cv2.COLOR_BGR2GRAY)
     end = time.time()
     print('opencv time: ', end - start)
-
-    cv2.imshow('Original image', image)
     cv2.imshow('Gray image', gray)
 
 
-
-    image2 = cv2.imread('src/image2.jpg')
+def time_average(image1):
     start = time.time()
-    copy1 = average(image2)
+    copy1 = average(image1)
     end = time.time()
     print('other realisation: ', end - start)
-
-    cv2.imshow('Original image', image2)
     cv2.imshow('average image', copy1)
 
 
+if __name__ == "__main__":
+    try:
+        image_path = sys.argv[1]
+        assert osp.isfile(image_path), '{} is not a file!'.format(image_path)
+    except IndexError:
+        raise AssertionError('image path must be specified!')
+
+
+    #image = cv2.imread('src/google.jpg') 
+    image = cv2.imread(image_path)
+    cv2.imshow('Original image', image)
+
+    time_cv(image)
+    time_average(image)
 
     cv2.waitKey(30000)
     cv2.destroyAllWindows()
